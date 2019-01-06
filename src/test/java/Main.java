606a77a8d5188de86470a79691536a1ba229dd45
epic of remnant <e185708@ie.u-ryukyu.ac.jp>
2019-01-06 18:59:51 +0900

    report6

---
+++ src/test/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main (String[] args){
+        Set set = new Set();
+    }
+}
---
+++ src/test/java/Stone.java
@@ -0,0 +1,11 @@
+public class Stone {
+    int x;
+    int y;
+    String color;
+    void putStone(int x, int y, String color){
+        this.x = x;
+        this.y =y;
+        this.color = color;
+    }
+
+}

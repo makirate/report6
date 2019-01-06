d093ba826a558bd6bb2043d8e3a606cf398a6a3b
epic of remnant <e185708@ie.u-ryukyu.ac.jp>
2019-01-06 23:39:30 +0900

    report6

--- src/test/java/Board.java
+++ src/test/java/Board.java
@@ -15,5 +15,8 @@ public class Board {
             }
         }
     }
+    public void putStone(){
+
+    }
 
 }
--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
-    Board board = new Board();
-    board.setBoard();
+    public static void main(String args[]){
+
+        Board board = new Board();
+        board.setBoard();
+        System.out.println();
+        System.out.print("どこに置きますか？");
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(str);
+    }
 
 }
